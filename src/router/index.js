31c19bec0a41fc9c70477b1f787bfe4fc12c9190
‘tiansuwen’ <791759913@qq.com>
2022-01-12 16:20:26 +0800

    route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Bar from '@/views/Bar.vue'
 Vue.use(VueRouter)
 
+// const Bar = { template: '<div>barDDDSDFSAFADSFADSFDASAS</div>' }
 const routes = [
   {
     path: '/',
@@ -17,7 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/bar',
+    name: 'Bar',
+    component: Bar
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Bar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bar">
+    <h1>This is an bar page{{ path }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+
+  data() {
+    return {};
+  },
+  computed: {
+    path() {
+      console.log("this.$route", this.$route);
+      return this.$route.path;
+    },
+  },
+};
+</script>
+
